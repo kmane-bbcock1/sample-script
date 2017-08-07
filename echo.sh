97e0fcd492c773afb88223a219fc8dbc1ae3eaae
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-07 19:18:46 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -3,4 +3,7 @@
 echo "hello"
 echo "this is runsh script sample-script repo test branch"
 #ecchho test
+sudo docker pull shippabledocker/sample_node_pvt:latest.40
+sudo docker pull 742038439709.dkr.ecr.us-west-2.amazonaws.com/sample_node
+sudo docker pull quay.io/revathird/samplenode_test
 echo "EOD!"
