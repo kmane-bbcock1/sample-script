57c1a126d4dd98921326f69d821430cc354f8a50
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-08 17:27:48 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -2,7 +2,7 @@
 
 echo "hello"
 
-sudo docker pull quay.io/revathird/testnodeimg:latest  #private image base image is old image
+sudo docker pull quay.io/revathird/docker:latest  #private image base image is old image
 sudo docker pull quay.io/revathird/test-repo:latest #public image base image is old image
 
 sudo docker pull quay.io/revathird/docker:officialimgtag #private image new image u16nodall:master 
