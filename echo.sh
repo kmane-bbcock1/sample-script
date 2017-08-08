1b29f5a4bc5471e68a8dc81df7747e1e8c79687b
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-08 16:33:08 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -2,12 +2,15 @@
 
 echo "hello"
 
-sudo docker pull shippabledocker/sample_node_pvt:master.37 #private image
-sudo docker pull shippabledocker/sample-node:latest.40 #public image
+sudo docker pull shippabledocker/sample_node_pvt:master.37 #private image old image
+sudo docker pull shippabledocker/sample-node:latest.40 #public image old image
+
+sudo docker pull shippabledocker/sample_node_pvt:officialimgtag #private image new image
+sudo docker pull shippabledocker/sample_node:officialimgtag #public image new image
 
 lsb_release -a
 docker --version
 docker info
 docker ps
 
-echo "EOD!"
+echo "EOF!"
