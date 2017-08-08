455dce728c65d1a086ea0bd7427c26404b0f7a09
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-08 14:28:21 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -4,4 +4,9 @@ echo "hello"
 
 sudo docker pull 742038439709.dkr.ecr.us-east-1.amazonaws.com/sample_php:officialimgtag #it is built using u16nodall
 
+lsb_release -a
+docker --version
+docker info
+docker ps
+
 echo "EOD!"
