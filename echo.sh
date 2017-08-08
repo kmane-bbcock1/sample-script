b31271bf1deed8f7f2906261554e4561c3a7f4b9
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-08 16:32:14 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -2,12 +2,14 @@
 
 echo "hello"
 
-sudo docker pull quay.io/revathird/testnodeimg:latest  #private image
-sudo docker pull quay.io/revathird/test-repo:latest #public image
+sudo docker pull quay.io/revathird/testnodeimg:latest  #private image base image is old image
+sudo docker pull quay.io/revathird/test-repo:latest #public image base image is old image
+
+sudo docker pull quay.io/revathird/docker:officialimgtag #private image new image u16nodall:master 
 
 lsb_release -a
 docker --version
 docker info
 docker ps
 
-echo "EOD!"
+echo "EOF!"
