6abc0149323f98cda348321e23109036276b497d
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-08 12:57:56 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -2,14 +2,7 @@
 
 echo "hello"
 
-echo "this is runsh script sample-script repo test branch"
-#ecchho test
-
-sudo docker pull shippabledocker/sample_node_pvt:latest.40 #private image
-sudo docker pull 742038439709.dkr.ecr.us-east-1.amazonaws.com/sample_php:officialimgtag #it is built using u16nodall
-sudo docker pull quay.io/revathird/docker:master.14 #private image
-
-##sudo docker pull quay.io/revathird/test-repo:latest #public image
-##sudo docker pull shippabledocker/sample-node:latest #public image
+sudo docker pull shippabledocker/sample_node_pvt:officialimgtag #private image
+sudo docker pull shippabledocker/sample_node:officialimgtag #public image
 
 echo "EOD!"
