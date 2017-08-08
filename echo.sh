5339a97b510763df6098740438765150660bb2cf
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-08 14:28:47 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -5,4 +5,9 @@ echo "hello"
 sudo docker pull quay.io/revathird/docker:officialimgtag #private image
 #sudo docker pull quay.io/revathird/test-repo:officialimgtag #public image
 
+lsb_release -a
+docker --version
+docker info
+docker ps
+
 echo "EOD!"
