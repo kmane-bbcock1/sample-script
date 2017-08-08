f3d0ca98ba27db549261b27c1c504810439f8849
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-08-08 14:28:35 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -5,4 +5,9 @@ echo "hello"
 sudo docker pull shippabledocker/sample_node_pvt:officialimgtag #private image
 sudo docker pull shippabledocker/sample_node:officialimgtag #public image
 
+lsb_release -a
+docker --version
+docker info
+docker ps
+
 echo "EOD!"
